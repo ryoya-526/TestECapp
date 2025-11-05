70381dfd5fd30316b09494e0e8f8156813c6c174
LAPTOP-1GMVB1LQ\ryoya.enomoto <ryoya.enomoto@sola-air.com>
2025-11-06 07:25:41 +0900

    ヘッダーの追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from '../components/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full',
+        
+    },
+    {
+        path: 'home',
+        component: Home,
+        
+    }
+
+];
